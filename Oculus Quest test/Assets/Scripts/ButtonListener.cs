5e2614834df483b6a290c7a97c390c206adab28f
yl74053 <q874982346@gmail.com>
2020-04-07 22:58:50 -0400

    Pinching Events
    Implementing Pinching Trigger Events.

--- Oculus Quest test/Assets/Scripts/ButtonListener.cs
+++ Oculus Quest test/Assets/Scripts/ButtonListener.cs
@@ -12,20 +12,31 @@ public class ButtonListener : MonoBehaviour
     public UnityEvent actionevent;
     public UnityEvent defaultevent;
 
+    public OVRHand lefthand;
+
+    public Material cur;
+    public Material[] materials;
+
+    bool eventtrigger;
+
+    public int count = 0;
+
     // Start is called before the first frame update
     void Start()
     {
         GetComponent<ButtonController>().InteractableStateChanged.AddListener(InitialEvent);
+        cur = materials[0];
+        eventtrigger = true;
     }
 
     void InitialEvent(InteractableStateArgs state)
     {
         if (state.NewInteractableState == InteractableState.ProximityState)
         {
-            proximityevent.Invoke();
+            GetComponent<MeshRenderer>().material = cur;
         } else if (state.NewInteractableState == InteractableState.ContactState)
         {
-            contactevent.Invoke();
+            GetComponent<MeshRenderer>().material = cur;
         } else if (state.NewInteractableState == InteractableState.ActionState)
         {
             actionevent.Invoke();
@@ -38,6 +49,34 @@ public class ButtonListener : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        bool isIndexFingerPinching = lefthand.GetFingerIsPinching(OVRHand.HandFinger.Index);
+        bool isMiddleFingerPinching = lefthand.GetFingerIsPinching(OVRHand.HandFinger.Middle);
+        bool isRingFingerPinching = lefthand.GetFingerIsPinching(OVRHand.HandFinger.Ring);
+        bool isPinkyFingerPinching = lefthand.GetFingerIsPinching(OVRHand.HandFinger.Pinky);
+
+        if (isIndexFingerPinching && eventtrigger)
+        {
+            cur = materials[1];
+            eventtrigger = false;
+        }
+        else if (isMiddleFingerPinching && eventtrigger)
+        {
+            cur = materials[2];
+            eventtrigger = false;
+        }
+        else if (isRingFingerPinching && eventtrigger)
+        {
+            cur = materials[3];
+            eventtrigger = false;
+        }
+        else if (isPinkyFingerPinching && eventtrigger)
+        {
+            cur = materials[4];
+            eventtrigger = false;
+        }
+        if (!eventtrigger && !isIndexFingerPinching && !isMiddleFingerPinching && !isRingFingerPinching && !isPinkyFingerPinching)
+        {
+            eventtrigger = true;
+        }
     }
 }
